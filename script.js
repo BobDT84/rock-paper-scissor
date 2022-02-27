bed414e025123e2ea4048ce8997d6b9611795205
Bob <bobdt84@gmail.com>
2022-02-27 10:01:53 -0600

    Created function playerSelect to prompt the player for their selection of 1, 2, or 3

--- script.js
+++ script.js
@@ -3,4 +3,21 @@ function computerSelect(){
     return select;
 }
 
-computerSelect()
+function playerSelect(){
+    console.log("Player make your selection:");
+    console.log("1. Rock");
+    console.log("2. Paper");
+    console.log("3. Scissor");
+    let select = parseInt(prompt("What is your selection?"));
+    if (select == 1 || select == 2 || select == 3) {
+        return select;
+    } else {
+        console.log("That was not a valid selection. Please try again.");
+        return playerSelect();
+    }
+}
+
+console.log("computerSelect");
+console.log(computerSelect());
+console.log("playerSelect");
+console.log(playerSelect());
